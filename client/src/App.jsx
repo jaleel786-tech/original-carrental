38daf00bcdac4d9a4a5cf8a05cc1cf4574d4fe68
Abdul Jaleel <you@example.com>
2025-08-28 12:36:54 -0700

    project completed

---
+++ client/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Navbar from './components/navbar'
+import Home from './pages/home'
+import CarDetails from './pages/CarDetails'
+import Cars from './pages/Cars'
+import Mybooking from './pages/Mybooking'
+import Footer from './components/Footer'
+import Layout from './pages/owner/Layout'
+import Dashboard from './pages/owner/Dashboard'
+import Addcar from './pages/owner/Addcar'
+import Managecars from './pages/owner/Managecars'
+import Managebooking from './pages/owner/Managebooking'
+import Login from './components/Login'
+import { Toaster } from 'react-hot-toast'
+import { useAppContext } from './context/AppContext'
+
+const App = () => {
+  const {showLogin} =useAppContext()
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+
+  return (
+    <>
+    <Toaster/>
+      {showLogin && <Login/>}
+
+      {!isOwnerPath && <Navbar/>}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<Mybooking />} />
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-car" element={<Addcar />} />
+          <Route path="manage-cars" element={<Managecars />} />
+          <Route path="manage-bookings" element={<Managebooking />} />
+        </Route>
+      </Routes>
+
+      {!isOwnerPath && <Footer />}
+    </>
+  )
+}
+
+export default App
+
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Featuredsection from '../components/Featuredsection'
+import Banner from '../components/Banner'
+import Testimonial from '../components/Testimonial'
+import Newsletter from '../components/Newsletter'
+
+const Home = () => {
+  return (
+    <>
+    <Hero/>
+    <Featuredsection/>
+    <Banner/>
+    <Testimonial/>
+    <Newsletter/>
+    
+
+        
+    </>
+  )
+}
+
+export default Home
